3204bd190694d6f5c0a2fb448534df2a3d794b20
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2021-10-21 14:41:58 +0300

    ChatBot 2-st stage

--- src/nix/education/java/chatbot/ChatBot.java
+++ src/nix/education/java/chatbot/ChatBot.java
@@ -1,8 +1,14 @@
 package nix.education.java.chatbot;
 
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
         System.out.println("Hello! My name is AnastasiaBot.");
         System.out.println("I was created in 2002.");
+        System.out.println("Please, remind me your name.");
+        Scanner in = new Scanner(System.in);
+        String name = in.next();
+        System.out.println("What a great name you have, " + name + "!");
     }
 }
