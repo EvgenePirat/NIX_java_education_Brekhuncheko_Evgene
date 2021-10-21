61026d8942911c0e6eeb86c5f58b421d721dab19
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2021-10-21 14:38:26 +0300

    ChatBot 1-st stage

---
+++ src/nix/education/java/chatbot/ChatBot.java
@@ -0,0 +1,8 @@
+package nix.education.java.chatbot;
+
+public class ChatBot {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is AnastasiaBot.");
+        System.out.println("I was created in 2002.");
+    }
+}
