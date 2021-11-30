a0531cf1de581fdb5e854fec01dce3555f998f57
EvgenePirat <y.y.brekhunchenko@student.khai.edu>
2021-11-30 23:27:14 +0300

    ChatBot 5-st stage

--- src/nix/education/java/chatbot/ChatBot.java
+++ src/nix/education/java/chatbot/ChatBot.java
@@ -3,13 +3,24 @@ package nix.education.java.chatbot;
 import java.util.Scanner;
 
 public class ChatBot {
+    static String name;
     public static void main(String[] args) {
+        greetings();
+        guessage();
+        printnumber();
+        secretquestion();
+        System.out.println("Goodbye, have a nice day!");
+    }
+    public static void greetings(){
         System.out.println("Hello! My name is AnastasiaBot.");
         System.out.println("I was created in 2002.");
         System.out.println("Please, remind me your name.");
         Scanner in = new Scanner(System.in);
-        String name = in.next();
+        name = in.next();
         System.out.println("What a great name you have, " + name + "!");
+    }
+    public static void guessage (){
+        Scanner in = new Scanner(System.in);
         System.out.println("Let me guess your age.");
         System.out.println("Enter remainders of dividing your age by 3,5 and 7");
         int n1 = in.nextInt();
@@ -17,10 +28,35 @@ public class ChatBot {
         int n3 = in.nextInt();
         int age = (n1 * 70 + n2 * 21 + n3 * 15) % 105;
         System.out.println("Your age is " + age + "; that`s a good time to start programming!");
+    }
+    public static void printnumber (){
+        Scanner in = new Scanner(System.in);
         System.out.println("Now I will prove to you that I can count to any number you want.");
         int n = in.nextInt();
         for (int i = 0; i <= n; i++) {
             System.out.println(i + " !");
+        }
+    }
+    public static void secretquestion (){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Сколько бит в двух байтах");
+        System.out.println("1) 4");
+        System.out.println("2) 8");
+        System.out.println("3) 16");
+        System.out.println("4) 32");
+        for (; ; ) {
+            System.out.print("Введите ответ: ");
+            int o = in.nextInt();
+            if (o == 1) {
+                System.out.println("Please, try again");
+            } else if (o == 2) {
+                System.out.println("Please, try again");
+            } else if (o == 3) {
+                System.out.println("Great.you right");
+                break;
+            } else if (o == 4) {
+                System.out.println("Please, try again");
+            }
         }
     }
 }
